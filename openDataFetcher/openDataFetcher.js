4db775f715074e07f0ad5695d0da9c824656ee51
Mario <mario@p058170.nat-gm.FH-Koeln.DE>
2019-03-07 18:52:21 +0100

    add Fetcher for cologne

---
+++ openDataFetcher/openDataFetcher.js
@@ -0,0 +1,80 @@
+const request = require('request')
+
+var cologneData_root = 'https://geoportal.stadt-koeln.de/arcgis/rest/services/'
+var format_string = '?f=pjson'
+
+exports.getCologneDatasetsList = (callback) => {
+
+    getCologneSubDatasets().then(function(response) {
+        callback(response);
+      }, function(error) {
+        callback(error);
+      })
+
+
+}
+
+exports.getCologneDataset = (name, callback) => {
+    
+}
+
+function getCologneSubDatasets() {
+    return new Promise((resolve, reject)=>{
+        getCologneMainDatasets().then(function(response) {
+            let combinedDatasets = [];
+            response.forEach(element => {
+                let serviceName = element.name;
+                let serviceTyp = element.type;
+                request(cologneData_root + '/' + serviceName + '/' +  serviceTyp + format_string, function (error, response, body) {
+                    if (response.statusCode == 200) {
+                        body = JSON.parse(body);
+                        let layers = body.layers;
+                        var layerArray = [];
+                        layers.forEach(element => {
+                            let layerID = layers.id;
+                            let layerName = layers.name;
+                            let layerObject = {
+                                "id": layerID,
+                                "name": layerName
+                            }
+                            layerArray.push(layerObject);
+                        });
+                        let datasetObject = {
+                            "name": serviceName,
+                            "layer": layerArray
+                        }
+                        combinedDatasets.push(datasetObject);
+
+                    } else {
+                        reject(error)
+                    }
+                });
+            });
+            resolve(combinedDatasets);
+          }, function(error) {
+            reject(error);
+          })
+    });
+    
+}
+
+function getCologneMainDatasets() {
+    return new Promise((resolve, reject) =>{
+    request(cologneData_root+format_string, function (error, response, body) {
+        if (response.statusCode == 200) {
+            body = JSON.parse(body);
+            var services = body.services;
+            var goodServices = [];
+            services.forEach((element,index) => {
+                if(!(element.type == 'GPServer' || element.type == 'GeocodeServer')){
+                    goodServices.push(element);
+                }
+            });
+            console.log(goodServices);
+            resolve(goodServices);
+        } else {
+            reject(error);
+        }
+    });
+    });
+}
--- routes/schema.js
+++ routes/schema.js
@@ -2,11 +2,14 @@
 const express = require("express");
 const schema = express.Router();
 const vocabulary = require('../vocabulary/vocabulary');
+const openDataFetcher = require('../openDataFetcher/openDataFetcher')
 
 schema.get('/', (req, res) => {
     vocabulary.findVocabularyByName(req.query.name, (response) => {
         res.json(response);
     });
 });
+
+openDataFetcher.getCologneDatasetsList(function(body) {console.log(body)});
 
 module.exports = schema;
