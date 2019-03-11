26a141270e80f7521c60e3ffd447cf30e5247fc8
Vimal Darius Seetohul <vimal.seetohul@gmail.com>
2019-03-11 02:08:02 +0100

    Implement schema.org parser
    Tested with some vocabularies
    Entire HTML Table interpreted
    Output as JSON

--- routes/schema.js
+++ routes/schema.js
@@ -5,9 +5,15 @@ const vocabulary = require('../vocabulary/vocabulary');
 const openDataFetcher = require('../openDataFetcher/openDataFetcher')
 
 schema.get('/', (req, res) => {
-    vocabulary.findVocabularyByName(req.query.name, (response) => {
+    vocabulary.getVocabularyMeta(req.query.name, (response) => {
         res.json(response);
     });
+});
+
+schema.get('/:name', (req, res) => {
+    vocabulary.getVocabulary(req.params.name, (response) => {
+        res.json(response);
+    })
 });
 
 openDataFetcher.getCologneDatasetsList(function(body) {console.log(body)});
--- vocabulary/vocabulary.js
+++ vocabulary/vocabulary.js
@@ -1,24 +1,97 @@
 const request = require('request');
 const jsonPath = require('jsonpath');
+const cheerio = require('cheerio');
+const schemaOrgUrl = 'https://schema.org';
 
-const schemaOrgVocabularyUrl = 'https://schema.org/docs/tree.jsonld';
-
-const options = {
-    uri: schemaOrgVocabularyUrl,
-    method: 'get',
-    headers: {
-        accept: 'application/json'
+exports.getVocabularyMeta = (name, callback) => {
+    const options = {
+        uri: schemaOrgUrl + '/docs/tree.jsonld',
+        method: 'get',
+        headers: {
+            accept: 'application/json'
+        }
     }
+    request(options, function (err, res, body) {
+        if (res.statusCode == 200) {
+            let result = JSON.parse(body);
+            if (name) result = jsonPath.query(result, '$..[?(@.name=="' + name + '")]');
+            callback(result);
+        } else {
+            callback({ error: `${options.uri} not responding` });
+        }
+    });
 }
 
-exports.findVocabularyByName = (name, callback) => {
+exports.getVocabulary = (name, callback) => {
+    const options = {
+        uri: schemaOrgUrl + '/' + name,
+        method: 'get',
+        headers: {
+            accept: 'application/json'
+        }
+    }
     request(options, function (err, res, body) {
-        if (res.statusCode == 200){
-            let result = JSON.parse(body);
-            if(name) result = jsonPath.query(result, '$..[?(@.name=="'+name+'")]');
-            callback(result);
-        } else{
-            callback({error: `${schemaOrgVocabularyUrl} not responding`});
+        if (res.statusCode == 200) {
+
+            // load the requested html
+            let $ = cheerio.load(body);
+
+            let vocabulary = {};
+            let vocabularyCols = [],
+                vocabularyProperties = [];
+
+            // get columns
+            $('.definition-table').first().find('thead tr th').each((i, element) => {
+                vocabularyCols[i] = $(element).text();
+            });
+
+            // get the 'Properties from <Vocabulary>' vocabulary 
+            $('.definition-table').first().find('tbody .supertype .supertype-name a').each((i, element) => {
+                vocabularyProperties[i] = $(element).text();
+            });
+
+            // get all the rows for each vocabulary
+            $('.definition-table').first().find('tbody.supertype').each((i, element) => {
+
+                // store amount of rows of current vocabulary
+                let totalLength = $(element).find('[property="rdfs:label"]').length;
+
+                let labels = [],
+                    expectedTypes = [],
+                    descriptions = [],
+                    vocabularyRows = [];
+
+                // get the labels
+                $(element).find('[property="rdfs:label"]').each((i, element) => {
+                    labels.push($(element).text());
+                });
+
+                // get the expected types
+                $(element).find('.prop-ect').each((i, element) => {
+                    let expectedType = $(element).text().trim().split(' or  ');
+                    expectedTypes.push(expectedType);
+                });
+
+                // get the descriptions
+                $(element).find('[property="rdfs:comment"]').each((i, element) => {
+                    descriptions.push($(element).text());
+                });
+
+                // push all rows for each vocabulary into vocabularyRows
+                for (let x = 0; x < totalLength; x++) {
+                    vocabularyRows.push({
+                        [[vocabularyCols[0]]]: labels[x],
+                        [[vocabularyCols[1]]]: expectedTypes[x],
+                        [[vocabularyCols[2]]]: descriptions[x]
+                    });
+                }
+
+                // set current vocabulary
+                vocabulary[vocabularyProperties[i]] = vocabularyRows;
+            });
+            callback(vocabulary);
+        } else {
+            callback({ error: `${options.uri} not responding` });
         }
     });
 }
