c4f96aa432ed09e4314cf6b808cad0b1ef214b11
vseetohu <vimal_darius.seetohul@smail.th-koeln.de>
2019-03-14 12:35:19 +0100

    Extend type matching
    Add db collection for mappings
    Optimize matching algorithm
    Downgrade db version

--- nlp/dataTypeMapper.js
+++ nlp/dataTypeMapper.js
@@ -1,20 +1,36 @@
 const nlp = require('natural');
 const schema = require('../schema/schema');
+const db = require('../utils/db');
 
 exports.findMatchingType = (type, callback) => {
-    schema.getSchemaMetaNames((result) => {
-        if (result.error) {
-            callback(result.error);
+    db.collection("mappings").findOne({ "type": type }, (err, match) => {
+        if (match) {
+            // return match if type already matched in database
+            callback(match.match);
         } else {
-            let indexBestMatch = 0;
-            let bestMatch = nlp.JaroWinklerDistance(result[indexBestMatch], type);
-            result.forEach((element, i) => {
-                if (nlp.JaroWinklerDistance(element, type) > bestMatch) {
-                    bestMatch = nlp.JaroWinklerDistance(element, type);
-                    indexBestMatch = i;
-                };
+            schema.getSchemaMetaNames((result) => {
+                if (result.error) {
+                    callback(result.error);
+                } else {
+                    let indexBestMatch = 0;
+                    let bestMatch = nlp.JaroWinklerDistance(result[indexBestMatch], type);
+                    result.forEach((element, i) => {
+                        if (nlp.JaroWinklerDistance(element, type) > bestMatch) {
+                            bestMatch = nlp.JaroWinklerDistance(element, type);
+                            indexBestMatch = i;
+                        };
+                    });
+                    // accept bestMatch only if string distance is greater or equal 0.85
+                    if (bestMatch >= 0.85) {
+                        db.collection("mappings").insertOne({ "type": type, "match": result[indexBestMatch] }, (err, match) => {
+                            callback(result[indexBestMatch]);
+                        });
+                    } else {
+                        // return original type if no match is found
+                        callback(type);
+                    }
+                }
             });
-            callback(result[indexBestMatch]);
         }
     });
 };
