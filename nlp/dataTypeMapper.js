894c0108a0c2c8e4b2e9f600c2d4904cb47c6304
vseetohu <vimal_darius.seetohul@smail.th-koeln.de>
2019-03-20 10:47:40 +0100

    Add NLP case insensivity

--- nlp/dataTypeMapper.js
+++ nlp/dataTypeMapper.js
@@ -14,16 +14,20 @@ exports.findMatchingType = (type, callback) => {
                 } else {
                     result = result.result;
                     let indexBestMatch = 0;
-                    let bestMatch = nlp.JaroWinklerDistance(result[indexBestMatch], type);
+                    let resultUpper = String(result[indexBestMatch]).toUpperCase();
+                    let typeUpper = String(type.toString()).toUpperCase();
+                    let bestMatch = nlp.JaroWinklerDistance(resultUpper, typeUpper);
                     result.forEach((element, i) => {
-                        if (nlp.JaroWinklerDistance(element, type) > bestMatch) {
-                            bestMatch = nlp.JaroWinklerDistance(element, type);
+                        resultUpper = String(element).toUpperCase();
+                        typeUpper = String(type.toString()).toUpperCase();
+                        if (nlp.JaroWinklerDistance(resultUpper, typeUpper) > bestMatch) {
+                            bestMatch = nlp.JaroWinklerDistance(resultUpper, typeUpper);
                             indexBestMatch = i;
                         };
                     });
                     // accept bestMatch only if string distance is greater or equal 0.85
                     if (bestMatch >= 0.85) {
-                        db.collection('mappings').insertOne({ "type": type, "match": result[indexBestMatch] }, (err, match) => {
+                        db.collection('mappings').insertOne({ "type": type, "match": result[indexBestMatch], "distance": bestMatch }, (err, match) => {
                             callback({ 'bestMatch': result[indexBestMatch] });
                         });
                     } else {
