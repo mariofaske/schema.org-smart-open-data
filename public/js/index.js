30a806642c8573399da0aec98ee698bd2283118a
Vimal Darius Seetohul <vimal.seetohul@gmail.com>
2019-03-20 02:24:02 +0100

    Extend client functions
    Add modal view for datasets
    Implement copy to clipboard for datasets
    Edit style
    Add loading message

--- public/js/index.js
+++ public/js/index.js
@@ -57,4 +57,47 @@ $('#inputLarge').keyup(delay(function (event) {
             }
         }
     }
-}, 800));
+}, 500));
+
+// adds application logic to every card
+$(document).on('click', '.card', function (e) {
+    let portal = $(this).find('.card-text')[0].innerText;
+    let dataset = $(this).find('.card-title')[0].innerText;
+    $('.modal-body').empty();
+    $('.modal-title').empty();
+
+    // display loading text
+    let count = 0;
+    let loadingText = setInterval(function () {
+        count++;
+        var dots = new Array(count % 5).join('.');
+        $('.modal-body').text('Loading dataset' + dots);
+    }, 300);
+
+    $('#myModal').modal({ show: true });
+    $.get('http://localhost:3000/portals/' + portal + '/' + dataset, function (response) {
+        clearInterval(loadingText);
+        let htmlJSON = '<code class="prettyprint"><pre>' + JSON.stringify(response, null, '\t') + '</pre></code>';
+        $('.modal-body').empty();
+        $('.modal-body').append(htmlJSON);
+        $('.modal-title').append(dataset);
+    });
+});
+
+// copy schema representation to clipboard
+$(document).on('click', '#copyToClipboard', function (e) {
+    // if-else for multi-browser support
+    if (document.selection) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById("jsonBody"));
+        range.select().createTextRange();
+        document.execCommand("copy");
+        document.selection.empty();
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById("jsonBody"));
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        window.getSelection().empty();
+    }
+})
